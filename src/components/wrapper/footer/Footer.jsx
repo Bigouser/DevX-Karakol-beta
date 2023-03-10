412aed81d45aff8af4a2f270a4ea330ffd6219c5
Elcho911 <98739225+Elkhan2003@users.noreply.github.com>
2023-03-10 16:47:35 +0600

    Update footer

--- src/components/wrapper/footer/Footer.jsx
+++ src/components/wrapper/footer/Footer.jsx
@@ -10,77 +10,48 @@ export const Footer = () => {
 		<div className="footer">
 			<div className="container">
 				<div className="content">
-					<div className="logo">
-						<img src={devxLogo} alt="iksu_devx_logo" width={100} />
-						<p>
-							Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Et
-							adipisci maiores at repellat, <br /> vel vitae non nulla aliquam
-							eaque id laboriosam ut odio <br /> quod eum sed esse corrupti,
-							dolorem alias.
-						</p>
+					<div className="footer__top">
+						<div className="block">
+							<img className="logo" src={devxLogo} alt="iksu_devx_logo" />
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+								adipisci maiores at repellat, vel vitae non ndivla aliquam eaque
+								id laboriosam ut odio quod eum sed esse corrupti, dolorem alias.
+							</p>
+						</div>
+						<div className="block">
+							<h6>Courses</h6>
+							<a href="#">Lorem</a>
+							<a href="#">Lorem</a>
+						</div>
+						<div className="block">
+							<h6>Karakol-IKSU</h6>
+							<a href="#">About Us</a>
+							<a href="#">FAQs</a>
+						</div>
+						<div className="block">
+							<h6>Contact Us</h6>
+							<a href="#">Lorem, ipsum.</a>
+							<a href="#">Lorem, ipsum.</a>
+						</div>
 					</div>
-					<div className="links">
-						<ul>
-							<li>
-								<a className="blue_line" href="#">
-									Courses
-								</a>
-							</li>
-							<li>
-								<a href="#">Lorem</a>
-							</li>
-							<li>
-								<a href="#">Lorem</a>
-							</li>
-						</ul>
-						<ul>
-							<li>
-								<a className="blue_line" href="#">
-									Karakol-IKSU
-								</a>
-							</li>
-							<li>
-								<a href="#">About Us</a>
-							</li>
-							<li>
-								<a href="#">FAQs</a>
-							</li>
-						</ul>
-						<ul>
-							<li>
-								<a className="blue_line" href="#">
-									Contact Us
-								</a>
-							</li>
-							<li>
-								<a href="#">Lorem, ipsum.</a>
-							</li>
-							<li>
-								<a href="#">Lorem, ipsum.</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-				<hr color="white" />
-				<div className="lower_content">
-					<span>Copyright © 2023 WEDEVX | Powered by WEDEVX</span>
-					<ul>
-						<li>
+					<hr />
+					<div className="footer__bottom">
+						<div className="copyright">
+							Copyright © 2023 WEDEVX | Powered by WEDEVX
+						</div>
+						<div className="icons">
 							<a href="">
 								<img src={instIcon} width={40} alt="inst" />
 							</a>
-						</li>
-						<li>
 							<a href="">
 								<img src={githubIcon} width={40} alt="github" />
 							</a>
-						</li>
-						<li>
 							<a href="">
 								<img src={discordIcon} width={40} alt="discord" />
 							</a>
-						</li>
-					</ul>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
