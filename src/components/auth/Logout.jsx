99448db22ed6e4ca28a93958ce538c7d8d810792
Elcho911 <98739225+Elkhan2003@users.noreply.github.com>
2023-03-22 19:49:56 +0600

    Fixed the bug with the icon

--- src/components/auth/Logout.jsx
+++ src/components/auth/Logout.jsx
@@ -1,14 +1,10 @@
 import React, { useContext } from "react";
 import { UserAuth } from "../../provider/AuthProvider.jsx";
-import { useTheme } from "../../hook/useTheme.jsx";
-import logout from "../../assets/logout.svg";
-import logoutDark from "../../assets/logout-dark.svg";
 
 import "./Auth.scss";
 import { TranslatorContext } from "../../provider/TranslatorProvider.jsx";
 
 export const Logout = () => {
-	const { isDark, setIsDark } = useTheme();
 	const { logOut, user } = UserAuth();
 	const { t } = useContext(TranslatorContext);
 
@@ -32,11 +28,12 @@ export const Logout = () => {
 				</div>
 			</div>
 			<div className="button" onClick={handleSignOut}>
-				{isDark ? (
-					<img src={logout} alt="logout" />
-				) : (
-					<img src={logoutDark} alt="logout" />
-				)}
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+					<path
+						fill="#fff"
+						d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z"
+					/>
+				</svg>
 			</div>
 		</div>
 	);
