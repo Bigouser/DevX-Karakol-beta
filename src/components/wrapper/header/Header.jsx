3c97caad86e403b3e7d313461539d94696b80711
Elcho911 <98739225+Elkhan2003@users.noreply.github.com>
2023-03-23 12:13:27 +0600

    Completely fixed the bug with displaying the Header for Safari

--- src/components/wrapper/header/Header.jsx
+++ src/components/wrapper/header/Header.jsx
@@ -80,18 +80,20 @@ export const Header = (props) => {
 										{links.map((link) =>
 											link.subRoutes ? (
 												<div className="dropdown" key={link.to}>
-													<span>
-														<pre>{link.text}</pre>
-														<svg
-															xmlns="http://www.w3.org/2000/svg"
-															aria-hidden="true"
-															focusable="false"
-															viewBox="0 0 24 24"
-															className="arrow__icon"
-														>
-															<path d="M12,16c-0.3,0-0.5-0.1-0.7-0.3l-6-6c-0.4-0.4-0.4-1,0-1.4s1-0.4,1.4,0l5.3,5.3l5.3-5.3c0.4-0.4,1-0.4,1.4,0s0.4,1,0,1.4l-6,6C12.5,15.9,12.3,16,12,16z" />
-														</svg>
-													</span>
+													<div className="icon">
+														<span>
+															<pre>{link.text}</pre>
+															<svg
+																xmlns="http://www.w3.org/2000/svg"
+																aria-hidden="true"
+																focusable="false"
+																viewBox="0 0 24 24"
+																className="arrow__icon"
+															>
+																<path d="M12,16c-0.3,0-0.5-0.1-0.7-0.3l-6-6c-0.4-0.4-0.4-1,0-1.4s1-0.4,1.4,0l5.3,5.3l5.3-5.3c0.4-0.4,1-0.4,1.4,0s0.4,1,0,1.4l-6,6C12.5,15.9,12.3,16,12,16z" />
+															</svg>
+														</span>
+													</div>
 
 													<div className="dropdown__content">
 														{link.subRoutes.map((subRoute) => (
